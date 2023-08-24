137631559dba9d3c9ed12168fe23a23e48e3f9a6
amandine <amandine.verstrepen@gmail.com>
2023-08-24 15:16:55 +0200

    maj requirements

--- script.py
+++ script.py
@@ -1,11 +1,19 @@
 from bs4 import BeautifulSoup as bs
 import requests
 
-#def bs_parse(html):
-#    return bs(html, 'lxml')
+url='https://books.toscrape.com'
 
-url='https://books.toscrape.com/'
-page = requests.get(url).text
+response = requests.get(url)
 
-soup=bs(page,'lxml')
-print(soup.get_text())
+if response.ok:
+    links = []
+    soup = bs(response.text, 'lxml')
+    h3s = soup.findAll('h3')
+    for h3 in h3s :
+        a = h3.find('a')
+        link = a['href']
+        links.append('https://books.toscrape.com/' + link)
+    print(links)
+
+
+
