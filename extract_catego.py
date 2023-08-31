d6f67b504872240501a0388cbb220778a8bf5c96
amandine <amandine.verstrepen@gmail.com>
2023-08-31 11:14:00 +0200

    Version 3 phase 3 - récupération des url des catégories

--- extract_catego.py
+++ extract_catego.py
@@ -8,12 +8,21 @@ response = requests.get(url)
 if response.ok:
     # création de mon object soup
     soup = bs(response.text, 'lxml')
-    # récupération des datas sous la balise a
-    masoupdea = soup.findAll('a')
-    # extraction de la partie text et ajout dans la liste link_catego
-    links_catego = []
-    for a in masoupdea :
-        link_catego = a['href']
-        links_catego.append('https://books.toscrape.com/' + link_catego)
 
-    print(links_catego)
+
+    #definition de la fonction recup_catego
+    def recup_catego(url) :
+        # récupération des datas sous la balise a
+        masoupdea = soup.findAll('a')
+        # extraction de la partie href et ajout dans la liste link_catego
+        links_catego = []
+        for a in masoupdea :
+            link_catego = a['href']
+            links_catego.append('https://books.toscrape.com/' + link_catego)
+        #suppression des données inutiles
+        del(links_catego[0:3])
+        del (links_catego[50:])
+        return links_catego
+
+
+print(recup_catego('url'))
